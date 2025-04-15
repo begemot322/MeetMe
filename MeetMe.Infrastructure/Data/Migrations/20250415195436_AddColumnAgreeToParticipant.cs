82b4ed34bea9067471e2889b76492a1705f7af41
Николай <kolya.zaitsev6163@gmail.com>
2025-04-15 23:55:27 +0400

    Миграция

---
+++ MeetMe.Infrastructure/Data/Migrations/20250415195436_AddColumnAgreeToParticipant.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetMe.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnAgreeToParticipant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAgreedWithFixedDate",
+                table: "Participants",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAgreedWithFixedDate",
+                table: "Participants");
+        }
+    }
+}
