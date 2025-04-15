194bb553d93d4094cfae59155b5bbdcd1b34376f
Николай <kolya.zaitsev6163@gmail.com>
2025-04-15 19:39:32 +0400

    Добавил событие. (one to many)  с участнком

---
+++ MeetMe.Domain/Entities/Event.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetMe.Domain.Entities;
+
+public class Event
+{
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string CreatorNickname { get; set; } 
+    public Guid Code { get; set; } = Guid.NewGuid();
+    public DateTime? FixedDate { get; set; } 
+    public bool IsDateRange { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    public ICollection<Participant> Participants { get; set; }
+}
