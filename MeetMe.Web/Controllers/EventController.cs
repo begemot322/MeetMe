cd4ea32d4cc2088223c12d8014ac843dd594c278
Николай <kolya.zaitsev6163@gmail.com>
2025-04-15 23:17:30 +0400

    Добавил create и success методы в контроллеры

---
+++ MeetMe.Web/Controllers/EventController.cs
@@ -0,0 +1,47 @@
+﻿using MeetMe.Application.Dtos;
+using MeetMe.Application.Services.Implementation;
+using MeetMe.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetMe.Web.Controllers;
+
+public class EventController : Controller
+{
+    private readonly EventService _eventService;
+
+    public EventController(EventService eventService)
+    {
+        _eventService = eventService;
+    }
+    
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateEventDto model)
+    {
+        if (!ModelState.IsValid)
+            return View(model);
+    
+        var eventCode = await _eventService.CreateEventAsync(model);
+        
+        return RedirectToAction("Success", new { code = eventCode });
+    }
+    
+    public async Task<IActionResult> Success(Guid code, string title)
+    {
+        var ev = await _eventService.GetEventByCodeAsync(code);
+
+        var vm = new SuccessViewModel
+        {
+            Code = ev.Code,
+            EventTitle = ev.Title
+        };
+
+        return View(vm);
+    }
+
+    
+}
