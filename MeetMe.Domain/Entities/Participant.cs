15dd75e2b30c2813bffe6d4c1ab73c97d1c70759
Николай <kolya.zaitsev6163@gmail.com>
2025-04-15 19:39:05 +0400

    Добавил участника. Связан с большим количество дат (one to many), и с событием

---
+++ MeetMe.Domain/Entities/Participant.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetMe.Domain.Entities;
+
+public class Participant
+{
+    public int Id { get; set; }
+    [Required]
+    public string Nickname { get; set; }
+    public bool IsCreator { get; set; }
+
+    public int EventId { get; set; }
+    public Event Event { get; set; }
+    
+    public ICollection<DateRange> DateRanges { get; set; } = new List<DateRange>();
+}
