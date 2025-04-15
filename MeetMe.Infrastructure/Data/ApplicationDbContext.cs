85ed6d44989e81c2425a13a606cfeb41a5f65c10
Николай <kolya.zaitsev6163@gmail.com>
2025-04-15 19:48:40 +0400

    Создал контекст.

---
+++ MeetMe.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MeetMe.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetMe.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Participant> Participants { get; set; }
+    public DbSet<DateRange> DateRanges { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
