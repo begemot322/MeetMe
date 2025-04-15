3d96918f00a9f886687bed7d62a301da52b54771
Николай <kolya.zaitsev6163@gmail.com>
2025-04-15 19:38:21 +0400

    Добавил сущность для дат. Связано с 1 участником

---
+++ MeetMe.Domain/Entities/DateRange.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetMe.Domain.Entities;
+
+public class DateRange
+{
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime StartDate { get; set; }
+
+    [Required]
+    public DateTime EndDate { get; set; }
+    
+    public int ParticipantId { get; set; }
+    public Participant Participant { get; set; }
+}
