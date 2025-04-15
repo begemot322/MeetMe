5261bd58b003a5c4875754c9c6b89f89c7e2c2b6
Николай <kolya.zaitsev6163@gmail.com>
2025-04-15 21:13:41 +0400

    Добавиль зависимость для сервиса

---
+++ MeetMe.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using MeetMe.Application.Services.Implementation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeetMe.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<EventService>();
+
+        return services;
+    }
+}
