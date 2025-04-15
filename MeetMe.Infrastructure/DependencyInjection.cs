cd3443e276ba558e5df983100cd94d4f5f64d56d
Николай <kolya.zaitsev6163@gmail.com>
2025-04-15 19:59:03 +0400

    Добавил сервис для подключения к базе данных

---
+++ MeetMe.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using MeetMe.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeetMe.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDatabase(configuration);
+
+        return services;
+    }
+    
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connString));
+        
+        return services;
+    }
+    
+}
