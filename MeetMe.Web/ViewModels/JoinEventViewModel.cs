cbc72fef6ca5abe62b2245a08fdfb853c8426442
Николай <kolya.zaitsev6163@gmail.com>
2025-04-15 23:58:09 +0400

    viewmodel для join

---
+++ MeetMe.Web/ViewModels/JoinEventViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetMe.Web.ViewModels;
+
+public class JoinEventViewModel
+{
+    [Required(ErrorMessage = "Код события обязателен")]
+    public Guid EventCode { get; set; }
+
+    [Required(ErrorMessage = "Никнейм обязателен")]
+    public string Nickname { get; set; }
+}
