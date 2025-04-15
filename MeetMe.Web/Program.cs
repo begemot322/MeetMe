8d621aee98f67aed953142ad91677ac3ebf10da8
Николай <kolya.zaitsev6163@gmail.com>
2025-04-15 19:59:30 +0400

    Добавил сервисы слоя infrastructur

--- MeetMe.Web/Program.cs
+++ MeetMe.Web/Program.cs
@@ -1,7 +1,10 @@
+using MeetMe.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
