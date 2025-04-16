070b7617337f785bb22b6166d987ea2037e2760e
Николай <kolya.zaitsev6163@gmail.com>
2025-04-16 04:52:38 +0400

    dto для создания участника

---
+++ MeetMe.Application/Dtos/CreateParticipantDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MeetMe.Application.Dtos;
+
+public class CreateParticipantDto
+{
+    public Guid EventCode { get; set; }
+    public string Nickname { get; set; }
+    public bool IsAgreedToFixedDate { get; set; }
+    public List<CreateDateRangeDto> DateRanges { get; set; } = new();
+}
