75e5243e304f24bcb14e13da8c5fb91e64d53e66
Николай <kolya.zaitsev6163@gmail.com>
2025-04-17 20:18:15 +0400

    Модели DTO для User

---
+++ MeetMe.Application/Dtos/Authentication/LoginUserRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetMe.Application.Dtos.Authentication;
+
+public class LoginUserRequest
+{
+    [Required(ErrorMessage = "Email обязателен.")]
+    [EmailAddress(ErrorMessage = "Некорректный формат Email.")]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
---
+++ MeetMe.Application/Dtos/Authentication/RegisterUserRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetMe.Application.Dtos.Authentication;
+
+public class RegisterUserRequest
+{
+    
+    [Required]
+    public string UserName { get; set; }
+    [Required(ErrorMessage = "Email обязателен.")]
+    [EmailAddress(ErrorMessage = "Некорректный формат Email.")]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
