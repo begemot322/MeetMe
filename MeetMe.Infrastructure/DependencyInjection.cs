abc02c33b5da94e70d4b8c45100ee4665d01de34
Николай <kolya.zaitsev6163@gmail.com>
2025-04-17 20:13:59 +0400

    Добавил сервис для аунтефикации

--- MeetMe.Infrastructure/DependencyInjection.cs
+++ MeetMe.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,17 @@
-﻿using MeetMe.Application.Common.Interfaces.Repositories;
+﻿using System.Security.Claims;
+using System.Text;
+using MeetMe.Application.Common.Interfaces.Identity;
+using MeetMe.Application.Common.Interfaces.Repositories;
 using MeetMe.Infrastructure.Data;
 using MeetMe.Infrastructure.Data.Repositories;
+using MeetMe.Infrastructure.Identity;
+using MeetMe.Infrastructure.Identity.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MeetMe.Infrastructure;
 
@@ -12,6 +20,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDatabase(configuration);
+        services.AddAuth(configuration);
 
         return services;
     }
@@ -23,7 +32,50 @@ public static class DependencyInjection
 
         services.AddScoped<IEventRepository, EventRepository>();
         services.AddScoped<IParticipantRepository, ParticipantRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
         
+        return services;
+    }
+    
+    private static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtOptions>(configuration.GetSection(nameof(JwtOptions)));
+
+        var jwtOptions = configuration.GetSection(nameof(JwtOptions)).Get<JwtOptions>();
+
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtOptions.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = jwtOptions.Audience,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions!.SecretKey)),
+                    RoleClaimType = ClaimTypes.Role,
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        context.Token = context.Request.Cookies["SecurityCookies"];
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        
+        services.AddScoped<IJwtProvider, JwtProvider>();
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+        services.AddScoped<IUserContext, UserContext>();
+
         return services;
     }
     
