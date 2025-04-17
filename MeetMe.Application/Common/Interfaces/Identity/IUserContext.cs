dbf55b6c1ec01016cc56d240072b89bc3113ef4b
Николай <kolya.zaitsev6163@gmail.com>
2025-04-17 20:11:07 +0400

    Получение id текущего пользователя

---
+++ MeetMe.Application/Common/Interfaces/Identity/IUserContext.cs
@@ -0,0 +1,6 @@
+﻿namespace MeetMe.Application.Common.Interfaces.Identity;
+
+public interface IUserContext
+{
+    int? GetCurrentUserId();
+}
---
+++ MeetMe.Infrastructure/Identity/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using MeetMe.Application.Common.Interfaces.Identity;
+using Microsoft.AspNetCore.Http;
+
+namespace MeetMe.Infrastructure.Identity;
+
+public class UserContext : IUserContext
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public UserContext(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public int? GetCurrentUserId()
+    {
+        var userIdClaim = _httpContextAccessor.HttpContext?.User?
+            .FindFirst("userId")?.Value;
+
+        return int.TryParse(userIdClaim, out var userId) ? userId : null;
+    }
+}
