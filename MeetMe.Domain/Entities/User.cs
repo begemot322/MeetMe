abbde0e0e37da6147dbffb53a290009fff3804fa
Николай <kolya.zaitsev6163@gmail.com>
2025-04-17 20:02:06 +0400

    Добавил миграцию для таблицы User

---
+++ MeetMe.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MeetMe.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string PasswordHash { get; set; }
+    public string Email { get; set; }
+    
+    public ICollection<Participant> Participants { get; set; } = new List<Participant>();
+}
---
+++ MeetMe.Infrastructure/Data/Migrations/20250417114230_AddUserTable.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetMe.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Participants",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participants_UserId",
+                table: "Participants",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Participants_Users_UserId",
+                table: "Participants",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Participants_Users_UserId",
+                table: "Participants");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Participants_UserId",
+                table: "Participants");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Participants");
+        }
+    }
+}
