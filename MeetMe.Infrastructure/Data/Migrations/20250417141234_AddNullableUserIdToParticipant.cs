77a1ee74cc807d255fb51aa9698ee228ed329ef4
Николай <kolya.zaitsev6163@gmail.com>
2025-04-17 20:02:45 +0400

    Потом сделал у Participant UserId nullable(не каждый участник будет иметь привязку к id)

---
+++ MeetMe.Infrastructure/Data/Migrations/20250417141234_AddNullableUserIdToParticipant.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetMe.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNullableUserIdToParticipant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Participants_Users_UserId",
+                table: "Participants");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Participants",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Participants_Users_UserId",
+                table: "Participants",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Participants_Users_UserId",
+                table: "Participants");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Participants",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Participants_Users_UserId",
+                table: "Participants",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
