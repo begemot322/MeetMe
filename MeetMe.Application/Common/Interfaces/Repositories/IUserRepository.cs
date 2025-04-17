489389bc8cd6df4b7784b0c0e3b945537374a8e2
Николай <kolya.zaitsev6163@gmail.com>
2025-04-17 20:11:55 +0400

    Репозиторий для юзера

---
+++ MeetMe.Application/Common/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using MeetMe.Domain.Entities;
+
+namespace MeetMe.Application.Common.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetByExpressionAsync(Expression<Func<User, bool>> predicate);
+
+    Task AddAsync(User user);
+
+    Task Update(User user);
+    Task<bool> ExistsAsync(Expression<Func<User, bool>> predicate);
+}
---
+++ MeetMe.Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq.Expressions;
+using MeetMe.Application.Common.Interfaces.Repositories;
+using MeetMe.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetMe.Infrastructure.Data.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public UserRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public async Task<User?> GetByExpressionAsync(Expression<Func<User, bool>> predicate)
+    {
+        return await _db.Users
+            .Where(predicate)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();    }
+
+    public async Task AddAsync(User user)
+    {
+        await _db.Users.AddAsync(user);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task Update(User user)
+    { 
+        _db.Users.Update(user);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<bool> ExistsAsync(Expression<Func<User, bool>> predicate)
+    {
+        return await _db.Users.AnyAsync(predicate);  
+    }
+}
