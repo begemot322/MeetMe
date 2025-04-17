024a8d4d9abe44effd34244f8e24c7245ae3c121
Николай <kolya.zaitsev6163@gmail.com>
2025-04-17 20:13:09 +0400

    Вынес сервисы web слоя

---
+++ MeetMe.Web/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿namespace MeetMe.Web;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddHttpContextAccessor();
+
+
+        return services;
+    }
+}
