0f997bc51e900d787653bbce7b83f3d9dc114faf
Николай <kolya.zaitsev6163@gmail.com>
2025-04-17 20:04:30 +0400

    Добавил таблицу Users

--- MeetMe.Infrastructure/Data/ApplicationDbContext.cs
+++ MeetMe.Infrastructure/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<Event> Events { get; set; }
     public DbSet<Participant> Participants { get; set; }
     public DbSet<DateRange> DateRanges { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
