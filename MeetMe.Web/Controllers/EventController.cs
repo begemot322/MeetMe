037b70cedc33f4222859ce1957e9e14adf9e6b1b
Николай <kolya.zaitsev6163@gmail.com>
2025-04-18 22:59:15 +0400

    Добавил методы для посчета лучшего времени

--- MeetMe.Web/Controllers/EventController.cs
+++ MeetMe.Web/Controllers/EventController.cs
@@ -1,6 +1,7 @@
 ﻿using MeetMe.Application.Common.Exceptions;
 using MeetMe.Application.Dtos;
 using MeetMe.Application.Services.Implementation;
+using MeetMe.Application.Services.Interfaces;
 using MeetMe.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,11 +9,15 @@ namespace MeetMe.Web.Controllers;
 
 public class EventController : Controller
 {
-    private readonly EventService _eventService;
-
-    public EventController(EventService eventService)
+    private readonly IEventService _eventService;
+    private readonly IParticipantService _participantService;
+    
+    public EventController(
+        IEventService eventService,
+        IParticipantService participantService)
     {
         _eventService = eventService;
+        _participantService = participantService;
     }
     
     public IActionResult Create()
@@ -26,15 +31,15 @@ public class EventController : Controller
         if (!ModelState.IsValid)
             return View(model);
     
-        var eventCode = await _eventService.CreateEventAsync(model);
+        var eventId = await _eventService.CreateEventAsync(model);
         TempData["success"] = "Вы успешно добавили событие";
         
-        return RedirectToAction("Success", new { code = eventCode });
+        return RedirectToAction("Success", new { eventId = eventId });
     }
     
-    public async Task<IActionResult> Success(Guid code, string title)
+    public async Task<IActionResult> Success(int eventId)
     {
-        var ev = await _eventService.GetEventByCodeAsync(code);
+        var ev = await _eventService.GetEventByIdAsync(eventId);
 
         var vm = new SuccessViewModel
         {
@@ -59,20 +64,20 @@ public class EventController : Controller
 
         var ev = await _eventService.GetEventByCodeAsync(model.EventCode);
         
-        return RedirectToAction("AddDates", new { code = model.EventCode, nickname = model.Nickname });
+        return RedirectToAction("AddDates", new { eventId = ev.Id, nickname = model.Nickname });
     }
     
     [HttpGet]
-    public async Task<IActionResult> AddDates(Guid code, string nickname)
+    public async Task<IActionResult> AddDates(int eventId, string nickname)
     {
-        var ev = await _eventService.GetEventByCodeAsync(code);
+        var ev = await _eventService.GetEventByIdAsync(eventId);
 
         ViewBag.Title = ev.Title;
         ViewBag.FixedDate = ev.FixedDate;
         
         var participantDto = new CreateParticipantDto
         {
-            EventCode = code,
+            EventId = eventId,
             Nickname = nickname,
             DateRanges = new List<CreateDateRangeDto>()
         };
@@ -86,7 +91,7 @@ public class EventController : Controller
         if (!ModelState.IsValid)
             return View(model);
         
-        await _eventService.AddParticipantAsync(model);
+        await _participantService.AddParticipantAsync(model);
         TempData["success"] = "Вы успешно добавили удобное время";
 
         return RedirectToAction("Index", "Home");
@@ -104,6 +109,29 @@ public class EventController : Controller
         {
             return View("EventNotFound", code);
         }
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> CalculateBestTime(int eventId)
+    {
+        try
+        {
+            var result = await _eventService.CalculateBestTime(eventId);
+            var ev = await _eventService.GetEventByIdAsync(eventId);
+        
+            var vm = new EventTimeSuggestionViewModel
+            {
+                ev = ev,
+                SuggestionDto = result
+            };
+            return View("TimeSuggestion", vm);
+        }
+        catch (Exception ex)
+        {
+            ModelState.AddModelError("", ex.Message);
+        }
+        var eventDetails = await _eventService.GetEventByIdAsync(eventId);
+        return View("Details", eventDetails);
     }
     
 }
