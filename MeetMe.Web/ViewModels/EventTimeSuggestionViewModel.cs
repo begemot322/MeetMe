25e6f1e89b7d50529651b83a6da8267da680e2bc
Николай <kolya.zaitsev6163@gmail.com>
2025-04-18 22:55:50 +0400

    добавил viewmodel

---
+++ MeetMe.Web/ViewModels/EventTimeSuggestionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MeetMe.Application.Services.Models;
+using MeetMe.Domain.Entities;
+
+namespace MeetMe.Web.ViewModels;
+
+public class EventTimeSuggestionViewModel
+{
+    public EventTimeSuggestion SuggestionDto { get; set; }
+    public Event ev { get; set; }
+}
