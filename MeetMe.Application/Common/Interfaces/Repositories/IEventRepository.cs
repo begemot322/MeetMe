14542b7a6f693d3aba6c40ca34528787ba7f63eb
Николай <kolya.zaitsev6163@gmail.com>
2025-04-18 22:56:46 +0400

    Добавил метод для получения по id

--- MeetMe.Application/Common/Interfaces/Repositories/IEventRepository.cs
+++ MeetMe.Application/Common/Interfaces/Repositories/IEventRepository.cs
@@ -4,6 +4,7 @@ namespace MeetMe.Application.Common.Interfaces.Repositories;
 
 public interface IEventRepository
 {
+    Task<Event?> GetByIdAsync(int id);
     Task<Event?> GetByCodeAsync(Guid code);
     Task AddAsync(Event ev);
 }
--- MeetMe.Infrastructure/Data/Repositories/EventRepository.cs
+++ MeetMe.Infrastructure/Data/Repositories/EventRepository.cs
@@ -13,12 +13,21 @@ public class EventRepository : IEventRepository
         _context = context;
     }
     
+    public async Task<Event?> GetByIdAsync(int eventId)
+    {
+        return await _context.Events
+            .Include(e => e.Participants)
+            .ThenInclude(p => p.DateRanges)
+            .FirstOrDefaultAsync(e => e.Id == eventId);    
+    }
+
     public async Task<Event?> GetByCodeAsync(Guid code)
     {
         return await _context.Events
             .Include(e => e.Participants)
             .ThenInclude(p => p.DateRanges)
-            .FirstOrDefaultAsync(e => e.Code == code);    }
+            .FirstOrDefaultAsync(e => e.Code == code);
+    }
 
     public async Task AddAsync(Event ev)
     {
