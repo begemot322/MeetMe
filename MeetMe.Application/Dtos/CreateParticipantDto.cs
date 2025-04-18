4c8a5abb74a505228fd3af19c5838544e2fb3a52
Николай <kolya.zaitsev6163@gmail.com>
2025-04-18 22:55:00 +0400

    Изменил dto, теперь через Id работает

--- MeetMe.Application/Dtos/CreateParticipantDto.cs
+++ MeetMe.Application/Dtos/CreateParticipantDto.cs
@@ -2,7 +2,7 @@
 
 public class CreateParticipantDto
 {
-    public Guid EventCode { get; set; }
+    public int EventId { get; set; }
     public string Nickname { get; set; }
     public bool IsAgreedToFixedDate { get; set; }
     public List<CreateDateRangeDto> DateRanges { get; set; } = new();
