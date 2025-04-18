08d0319ae6951adeacf77ca6d53319b7bd9a784d
Николай <kolya.zaitsev6163@gmail.com>
2025-04-18 22:57:30 +0400

    интерфейс для посчета лучшего времени

---
+++ MeetMe.Application/Services/Interfaces/IEventTimeCalculationService.cs
@@ -0,0 +1,8 @@
+﻿using MeetMe.Application.Services.Models;
+
+namespace MeetMe.Application.Services.Interfaces;
+
+public interface IEventTimeCalculationService
+{
+    Task<EventTimeSuggestion> CalculateBestTime(int eventId);
+}
