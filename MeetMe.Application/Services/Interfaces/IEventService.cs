1d7139acaa21148006a89654bfc3f71beba737e0
Николай <kolya.zaitsev6163@gmail.com>
2025-04-18 22:59:32 +0400

    Интерфейс для event service

---
+++ MeetMe.Application/Services/Interfaces/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using MeetMe.Application.Dtos;
+using MeetMe.Application.Services.Models;
+using MeetMe.Domain.Entities;
+
+namespace MeetMe.Application.Services.Interfaces;
+
+public interface IEventService
+{
+    Task<int> CreateEventAsync(CreateEventDto EventDto);
+    Task<Event> GetEventByIdAsync(int id);
+    Task<Event> GetEventByCodeAsync(Guid code);
+    Task<EventTimeSuggestion> CalculateBestTime(int eventId);
+}
