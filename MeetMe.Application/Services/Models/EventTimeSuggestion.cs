3add1d72689048aebcd61924a391266e9a1c51fe
Николай <kolya.zaitsev6163@gmail.com>
2025-04-18 22:53:16 +0400

    Добавил модельки для работы в сервисах

---
+++ MeetMe.Application/Services/Models/EventTimeSuggestion.cs
@@ -0,0 +1,8 @@
+﻿namespace MeetMe.Application.Services.Models;
+
+public class EventTimeSuggestion
+{
+    public DateTime SuggestedStart { get; set; }
+    public DateTime SuggestedEnd { get; set; }
+    public int EventId { get; set; }
+}
---
+++ MeetMe.Application/Services/Models/TimeSlot.cs
@@ -0,0 +1,7 @@
+﻿namespace MeetMe.Application.Services.Models;
+
+public class TimeSlot
+{
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+}
